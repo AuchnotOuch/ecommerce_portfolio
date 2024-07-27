6cf58e5fe059954e2018c61843119b3247712580
Alex Auch <alexsamuelauch@gmail.com>
2024-07-26 18:08:25 -0600

    added some styling to landing and sidebar:

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Box,
     Button,
@@ -26,28 +26,55 @@ const Sidebar = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const btnRef = React.useRef();
 
+    // State to control the visibility of the menu button
+    const [isButtonVisible, setButtonVisible] = useState(true);
+
+    // Function to handle opening the drawer
+    const handleOpen = () => {
+        onOpen();
+        setButtonVisible(false);
+    };
+
+    // Function to handle closing the drawer
+    const handleClose = () => {
+        onClose();
+        setButtonVisible(true);
+    };
+
     return (
         <>
-            <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
-                <HamburgerIcon />
-            </Button>
+            {isButtonVisible && (
+                <Button
+                    ref={btnRef}
+                    colorScheme="teal"
+                    onClick={handleOpen}
+                    position="fixed"
+                    top="1rem"
+                    left="1rem"
+                    zIndex="2000"
+                >
+                    <HamburgerIcon />
+                </Button>
+            )}
             <Drawer
                 isOpen={isOpen}
                 placement="left"
-                onClose={onClose}
+                onClose={handleClose}
                 finalFocusRef={btnRef}
+                returnFocusOnClose={false}
+                preserveScrollBarGap
             >
                 <DrawerOverlay />
-                <DrawerContent>
-                    <DrawerCloseButton />
+                <DrawerContent bg="rgba(255, 255, 255, 0.5)" backdropFilter="blur(10px)" zIndex="3000" color="white">
+                    <DrawerCloseButton onClick={handleClose} />
                     <DrawerHeader>Navigation</DrawerHeader>
 
                     <DrawerBody>
                         <VStack spacing={4} align="stretch">
-                            <Link as={RouterLink} to="/" onClick={onClose}>
+                            <Link as={RouterLink} to="/" onClick={handleClose}>
                                 Home
                             </Link>
-                            <Link as={RouterLink} to="/shop-all" onClick={onClose}>
+                            <Link as={RouterLink} to="/shop-all" onClick={handleClose}>
                                 Shop All
                             </Link>
                             <Accordion allowToggle>
@@ -61,17 +88,17 @@ const Sidebar = () => {
                                     <AccordionPanel pb={4}>
                                         <List spacing={2}>
                                             <ListItem>
-                                                <Link as={RouterLink} to="/category/category1" onClick={onClose}>
+                                                <Link as={RouterLink} to="/category/category1" onClick={handleClose}>
                                                     Category 1
                                                 </Link>
                                             </ListItem>
                                             <ListItem>
-                                                <Link as={RouterLink} to="/category/category2" onClick={onClose}>
+                                                <Link as={RouterLink} to="/category/category2" onClick={handleClose}>
                                                     Category 2
                                                 </Link>
                                             </ListItem>
                                             <ListItem>
-                                                <Link as={RouterLink} to="/category/category3" onClick={onClose}>
+                                                <Link as={RouterLink} to="/category/category3" onClick={handleClose}>
                                                     Category 3
                                                 </Link>
                                             </ListItem>
@@ -79,13 +106,13 @@ const Sidebar = () => {
                                     </AccordionPanel>
                                 </AccordionItem>
                             </Accordion>
-                            <Link as={RouterLink} to="/cart" onClick={onClose}>
+                            <Link as={RouterLink} to="/cart" onClick={handleClose}>
                                 View Cart
                             </Link>
-                            <Link as={RouterLink} to="/sign-in" onClick={onClose}>
+                            <Link as={RouterLink} to="/sign-in" onClick={handleClose}>
                                 Sign In
                             </Link>
-                            <Link as={RouterLink} to="/privacy" onClick={onClose}>
+                            <Link as={RouterLink} to="/privacy" onClick={handleClose}>
                                 Privacy Info
                             </Link>
                         </VStack>
